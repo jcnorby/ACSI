4db90e94ea965bd67080133e36779d492673ed41
Joe Norby <jnorby@andrew.cmu.edu>
2018-10-30 23:11:38 -0400

    Add waypoint tracking

--- Project/forwardInt.m
+++ Project/forwardInt.m
@@ -1,6 +1,6 @@
 function [x,u] = forwardInt(x0,u_ff, K, N,dt, x_bar)
 x = x0;
-for ii = 1:N
+for ii = 1:N-1
     u_fb(:,ii) = -K(:,:,ii)*(x(:,ii) - x_bar);
     u(:,ii) = u_ff(:,ii) + u_fb(:,ii);
     x(:,ii+1) = f(x(:,ii),u(:,ii),dt);
--- Project/intermediateCost.m
+++ Project/intermediateCost.m
@@ -1,7 +1,14 @@
-function L = intermediateCost(x,u)
+function L = intermediateCost(t,x,u, x_wp)
 
-Q = 0.1*eye(12);
+Q = 1*eye(12);
 R = 0.5*eye(6);
+Wp = 1000*eye(12);
+
+rho = 200;
+
+% x_wp = [0.5;0.5;1;0;0;0;
+%     1;1;0;0;0;0];
+t_wp = 1;
 
 % x_bar = [1;1;1;0;0;0;
 %     0;0;0;0;0;0];
@@ -10,7 +17,10 @@ R = 0.5*eye(6);
 % dx = x - x_bar;
 % du = u - u_bar;
 
-% dx = x;
-% du = u;
+dx = x;
+du = u;
 
-L = x'*Q*x + u'*R*u;
+xhat = x - x_wp;
+W = xhat'*Wp*xhat*sqrt(rho/(2*pi))*exp(-rho/2*(t - t_wp)^2);
+
+L = dx'*Q*dx + du'*R*du + W;
