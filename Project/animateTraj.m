79ab05a7ed61e4c37095e85a223ac4bc2630ebf2
Joe Norby <jnorby@andrew.cmu.edu>
2018-12-03 15:58:08 -0500

    Add visualization of thrust vector

--- Project/animateTraj.m
+++ Project/animateTraj.m
@@ -1,4 +1,4 @@
-function animateTraj(x,xf,t_wp,x_wp,dt,N,T)
+function animateTraj(x,u,xf,t_wp,x_wp,dt,N,T)
 
 % Initialize plotting
 quad_w = 0.092;
@@ -14,17 +14,23 @@ ylabel('y')
 zlabel('z')
 quadrotor = [];
 h_wp = [];
+thrust = [];
 grid on
 
 while(1)
     t = 0;
-    for ii = 1:N
+    for ii = 1:N-1
         a = tic;
+        
+        thrustVec = (eul2rotm(flipud(x(4:6,ii))'))*[0;0;u(1,ii)];
+        
         addpoints(h,x(1,ii),x(2,ii),x(3,ii));
         delete(quadrotor) % Comment out to save snapshots
         delete(h_wp) %
+        delete(thrust);
         quadrotor = plotrotcube([quad_w quad_l quad_h],[x(1,ii) - quad_w/2,x(2,ii)- quad_l/2,x(3,ii)- quad_h/2],.8,[1 0 0], x(4,ii),x(5,ii),x(6,ii));
         %         quadrotor = plotrotcube([quad_w quad_l quad_h],[x(1,ii) - quad_w/2,x(2,ii)- quad_l/2,x(3,ii)- quad_h/2],.8,[1 0 0],pi/6,pi/6,pi/6);
+        thrust = quiver3(x(1,ii),x(2,ii),x(3,ii),thrustVec(1),thrustVec(2),thrustVec(3), '-b');
         if t < t_wp || T - t < 0.2
             h_wp = plot3(x_wp(1,ii), x_wp(2,ii), x_wp(3,ii), 'or', 'MarkerSize', 20, 'LineWidth', 3);
         else
