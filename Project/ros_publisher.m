8aeb611adfccbd13b9f973b89eeb41db09a49f8c
Brian Kim <brikim13@gmail.com>
2018-12-10 00:56:20 -0500

    through thrown hoop

--- Project/ros_publisher.m
+++ Project/ros_publisher.m
@@ -43,9 +43,9 @@ quad_orient = [0;0;0];
 quad_ang_vel = [0;0;0];
 msg_launch_data = false;
 
-x_start = [0.6;2.0;2.3;0;0;0
+x_start = [0.0;2.0;2.0;0;0;0
     0;0;0;0;0;0];
-x_end = [1.2;-1.3;1.5;0;0;0
+x_end = [0;-1.0;2.0;0;0;0
     0;0;0;0;0;0];
 
 % x_end = [0.6;0;1;0;0;0
@@ -66,6 +66,8 @@ u = 9.81*0.034;
 umax = 0.4;
 t_current=0;
 trajNum = 0;
+trial = ['1_',datestr(datetime('now'))];
+
 while(1)    
     if ~isempty(cf_sub.LatestMessage)
         msg_cf = cf_sub.LatestMessage;    
@@ -116,15 +118,16 @@ while(1)
 
     t = (curTime.Sec + curTime.Nsec / 1e9) - (secs + nsecs/1e9);
 
-    quad_vel
-    quad_ang_vel
+%     quad_vel;
+%     quad_ang_vel;
 %     quad_vel = [0;0;0];
 %     quad_ang_vel = [0;0;0];
     x0 = [quad_pos;quad_orient;quad_vel;quad_ang_vel];
-    if launch_flag && ((t-t_compute) >=0.2) && t < 1.45
+    if launch_flag && ((t-t_compute) >=0.2) && t < 0.65
         x0
+        hoop_vel(1:2) = [0;0];
         [xtraj,K,u, t_wp, x_wp] = computeSLQTrajHoop_mex(t,N,dt,x0,xf,umax,hoop_pos, hoop_vel, hoop_accel, launch_flag);
-        save(['trajData_', num2str(trajNum)], 't','x0','xtraj', 't_wp', 'x_wp');
+        save(['trajData_', trial, '_',num2str(trajNum)], 't','x0','xtraj', 't_wp', 'x_wp');
         if ~isnan(x(end, end))
             x = xtraj;
         end
