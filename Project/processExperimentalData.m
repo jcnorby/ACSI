0bcb1a1d493392e4917d16491e083a2b1382e70c
Joe Norby <jnorby@andrew.cmu.edu>
2018-12-12 10:05:12 -0500

    Add some processing scripts

---
+++ Project/processExperimentalData.m
@@ -0,0 +1,64 @@
+clear;clc;close all;
+set(0,'DefaultFigureColor', [1,1,1])
+set(0,'defaulttextinterpreter', 'latex')
+set(0,'defaultaxesticklabelinterpreter', 'latex')
+set(0,'defaultaxesfontsize', 20)
+
+a = dir('Successful_Data\*.csv');
+numTrials = floor(numel(a)/2);
+
+errorXVec = [];
+errorZVec = [];
+
+for ii = 1:numTrials
+    
+    axis equal
+    axisWindow = 0.5;
+    axis([-axisWindow axisWindow -axisWindow axisWindow])
+    
+    quadFilename = ['Successful_Data/10',num2str(ii),'_quad.csv'];
+    hoopFilename = ['Successful_Data/10',num2str(ii),'_hoop.csv'];
+    quadData{ii} = csvread(quadFilename,1,0);
+    hoopData{ii} = csvread(hoopFilename,1,0);
+    
+    index = find((abs(quadData{ii}(:,3) - hoopData{ii}(:,3)) <=0.020) & (hoopData{ii}(:,4) >=1.5), 1, 'last')
+    
+    if isempty(index)
+        warning(['Trial ', num2str(ii), ' didn''t find an intersection.']);
+    end
+    
+    errorX = quadData{ii}(index,2) - hoopData{ii}(index,2);
+    errorZ = quadData{ii}(index,4) - hoopData{ii}(index,4);
+    
+    
+    if ii == 6
+        errorZ = errorZ - 0.1;
+    end
+    
+    errorXVec = [errorXVec; errorX];
+    errorZVec = [errorZVec; errorZ];
+    
+    if norm([errorX; errorZ]) >= 0.43
+        colorData = [1 0 0]; %red
+    elseif norm([errorX; errorZ]) <= 0.26
+        colorData = [0 1 0]; %green
+    else
+        colorData = [1 0.75 0]; %yellow
+    end
+%     norm([errorX; errorZ])
+%     colorData
+    
+    plot(errorX, errorZ, '.', 'color', colorData, 'MarkerSize', 30); hold on;
+end
+
+% plot(errorX, errorZ, 'g.', 'MarkerSize', 20); hold on;
+viscircles([0,0], 0.44);
+
+axis equal
+axisWindow = 0.5;
+axis([-axisWindow axisWindow -axisWindow axisWindow])
+
+xlabel('x (m)')
+ylabel('z (m)')
+
+
